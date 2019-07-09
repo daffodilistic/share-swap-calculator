2712e954ae19ff468264d1d51a6846a5ad605cbf
Soh Thiam Hing <daffodilistic@gmail.com>
2019-07-09 11:01:30 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# share-swap-calculator
+# Share Swap Calculator
 A simple VueJS app to calculate minimum amount of shares to avoid getting odd lots
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Share Merger/Swap Calculator</h1>
+    <br />
+    <div class="col mx-auto">
+      <div class="row">
+        <div class="col">
+          <h4 class="text-center">Merger/Swap Ratio</h4>
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            v-model="swapRatio"
+            placeholder="0.7942"
+          />
+        </div>
+      </div>
+      <br />
+      <div class="row">
+        <div class="col">
+          <h4 class="text-center">Board Lot Size</h4>
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            v-model="lotSize"
+            placeholder="100"
+          />
+        </div>
+      </div>
+      <hr />
+      <div class="row">
+        <div class="col">
+          <h4 class="text-center">Minimum Pre-Swap Amount</h4>
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            disabled
+            class="form-control"
+            v-model="preSwapAmount"
+          />
+        </div>
+      </div>
+      <br />
+      <div class="row">
+        <div class="col">
+          <h4 class="text-center">Post-Swap Amount</h4>
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            disabled
+            class="form-control"
+            v-model="preSwapAmount"
+          />
+        </div>
+      </div>
+      <br />
+      <div class="row">
+        <div class="col col-md-4 mx-auto">
+          <button
+            type="button"
+            v-on:click="calculate"
+            class="btn btn-block btn-primary"
+          >
+            Calculate
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      swapRatio: 0.7942,
+      lotSize: 100,
+      preSwapAmount: 0,
+      postSwapAmount: 0
+    };
+  },
+  methods: {
+    calculate() {
+      // console.log("Calculating...");
+
+      this.preSwapAmount = 0;
+      var counter = 1;
+      var found = false;
+
+      while (!found) {
+        this.preSwapAmount = counter * this.lotSize;
+        // console.log("preSwapAmount is " + this.preSwapAmount);
+        if ((this.preSwapAmount * this.swapRatio) % this.lotSize == 0) {
+          this.postSwapAmount = this.preSwapAmount * this.swapRatio;
+          // console.log("postSwapAmount is " + this.postSwapAmount);
+          found = true;
+        }
+        counter++;
+      }
+      // console.log("Done!");
+    }
+  }
+};
+</script>
+
+<style></style>
